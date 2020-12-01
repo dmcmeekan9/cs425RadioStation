f7dda684b79ef0e649643a0f86496f5dc00304a2
dmcmeekan9 <dennismcmeekan@gmail.com>
2020-11-30 21:19:47 -0600

    Angular is working with LAMP

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/**
+ * WIU Web Radio Application
+ */
+var app = angular.module('basicWebRadio', [
+  'ngRoute'
+]);
+
+/**
+ * Route Configuration
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Page Controller
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
