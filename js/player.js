0cdc134b736cee280bcea0a2b83cd132918f7a14
Brandon Ahrens <b-ahrens@wiu.edu>
2020-12-07 23:55:55 -0600

    Fully functional | Audio is played properly and loops | A check was also added to display an alert if a password for the database is not entered | Artist and Song names are displayed correctly | Refactored javascript in home.html into seperate .js files | added player.js functionality

---
+++ js/player.js
@@ -0,0 +1,55 @@
+/**
+ * Uses song information from songInfo.js to make a simple media player that will
+ * re loop through the songs when they are done playing.
+ * Uses variables from songInfo.js:
+ *      songNames[]
+ *      songArtists[] 
+ *      songMP3FilePaths[]
+ */
+
+const song = document.querySelector('#audio_controller'); //the audio object in home.html
+const artist = document.querySelector('#nameOfArtist'); //the element where the artist will be displayed.
+const songName = document.querySelector('#nameOfSong'); //the element where the name of the song will be displayed.
+
+
+index = 0; //index to be used for songMP3FilePaths array.
+
+//set the first song to play and display the artist and song name.
+song.src = songMP3FilePaths[0];
+artist.innerHTML = "Artist: ".concat(songArtists[0]);
+songName.innerHTML = "Song: ".concat(songNames[0]);
+
+function play(){
+    //re get the song to play
+    const song = document.querySelector('#audio_controller');
+
+    song.play();
+}
+
+
+//if at end of song, go to next song.
+song.addEventListener('ended', function(){
+    nextSong();
+});
+
+//Will go to next song when current song is done playing by increasing index to use in
+//songMP3FilePaths array and setting that song file path as the src for the song audio object.
+function nextSong(){
+    index++;
+    
+    //check to see if last song ended. If so, loop back to first song
+    //by resetting index back to 0.
+    if (index > 2){
+        index = 0;
+    }
+
+    //set next song
+    song.src = songMP3FilePaths[index];
+
+    //set artist and song name to display
+    artist.innerHTML = "Artist: ".concat(songArtists[index]);
+    songName.innerHTML = "Song: ".concat(songNames[index]);
+
+    //play song
+    play();
+} 
---
+++ js/songInfo.js
@@ -0,0 +1,63 @@
+/**
+ * This script will get the song information from the serverside and put it in arrays
+ * that can be accessed globally.
+ * 
+ * gotSongs will be used to check if database connection was successful or not.
+ * 
+ * Uses XMLHttpRequest
+ */
+
+//arrays to store song info
+var songNames = []; 
+var songArtists = [];
+var songLengths = []; //will be in 00:04:26.000 format as example.
+var songMP3s = []; //used to store just the mp3 names of the songs.
+var songMP3FilePaths = []; //used to store the full file path of the songs on the server.
+var gotSongs = false; //used to know if was unable to connect to database to get songs.
+
+//create the listener for alert
+function reqListener(){
+    console.log(this.responseText);
+}
+
+//new request object
+var oReq = new XMLHttpRequest();
+
+//action of what will happen when getting data from getSongs.php if successful.
+oReq.onload = function(){
+    //data is in this.responseText.
+    //We will handle what we need to do with this data here.
+
+    //parse JSON string into javascript array of objects. Song 1 is songs[0]..., etc.
+    var songs = JSON.parse(this.responseText);
+
+
+    //place song data into seperate arrays.
+    songs.forEach(function (song) {
+        songNames.push(song.name);
+        songArtists.push(song.artist);
+        songLengths.push(song.length);
+        songMP3s.push(song.mp3);
+        songMP3FilePaths.push("assets/songs/".concat(song.mp3));
+    });
+
+    gotSongs = true;
+    
+};
+
+//initialize the request to get data from getSongs.php
+//is using synchronous with third param being false instead of true 
+//and will prevent everything else below this line from
+//executing until request is complete.
+//This ensures that the song data is able to be accessed outside the scope of oReq.onload
+//in a global way.
+//If the request does fail, an alert will be displayed as gotSongs will stay false.
+oReq.open("get", "php/getSongs.php", false);
+
+//send the request for data
+oReq.send();
+
+//will check to see if connection to database was successful and song information was gotten.
+if (gotSongs == false){
+    alert("Check to see if password is set to connect to database in php/connectToDB");
+}
